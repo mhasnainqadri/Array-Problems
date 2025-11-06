6d3efe5622f63a3af24f1b085889008bac073a80
m_hasna1n <hasnainqadri9c@gmail.com>
2025-11-06 21:22:48 +0500

    Add files via upload

---
+++ 19.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int SIZE;
+    printf("Enter the size of square matrix : ");
+    scanf("%d",&SIZE);
+    int matrix[SIZE][SIZE];
+    printf("Enter the elements of A matrix (2D array) :\n");
+    for(int i=0;i<SIZE;i++)
+    {
+        for(int j=0;j<SIZE;j++)
+        {
+            scanf("%d",&matrix[i][j]); // taking A matrix input
+        }
+    }
+    int matrix1[SIZE][SIZE];
+    printf("Enter the elements of B matrix (2D array) :\n");
+    for(int i=0;i<SIZE;i++)
+    {
+        for(int j=0;j<SIZE;j++)
+        {
+            scanf("%d",&matrix1[i][j]); // taking B matrix input
+        }
+    }
+    int matrix2[SIZE][SIZE]; // Sum Matrix
+    for(int i=0;i<SIZE;i++)
+    {
+        for(int j=0;j<SIZE;j++)
+        {
+            matrix2[i][j]=matrix[i][j]+matrix1[i][j]; // adding matrix A and matrix B and storing sum in matrix2
+        }
+    }
+    printf("Sum of Matrices is :\n");
+    for(int i=0;i<3;i++)
+    {
+        for(int j=0;j<3;j++)
+        {
+            printf("%d ",matrix2[i][j]); // Displaying matrix2
+        }
+        printf("\n"); // line break
+    }
+    getch();
+}
